547fe7d8e3ab39d6f37570159bbb039eebdae96c
raihants <raihantaufiksuryana0511@gmail.com>
2024-10-28 08:52:18 +0700

    first commit

---
+++ hapus-siswa.php
@@ -0,0 +1,14 @@
+<?php
+
+include('config.php');
+
+//get id
+$id = $_GET['id'];
+
+$query = "DELETE FROM tbl_siswa WHERE id_siswa = '$id'";
+
+if($connection->query($query)) {
+    header("location: index.php");
+} else {
+    echo "DATA GAGAL DIHAPUS!";
+}
---
+++ simpan-siswa.php
@@ -0,0 +1,14 @@
+<?php
+    include ('config.php');
+
+    $nisn = $_POST['nisn'];
+    $nama_lengkap = $_POST['nama_lengkap'];
+    $alamat = $_POST['alamat'];
+
+    $query = "INSERT INTO tbl_siswa (nisn,nama_lengkap,alamat) VALUES('$nisn','$nama_lengkap','$alamat')";
+
+    if ($connection->query($query)){
+        header("location: index.php");
+    } else {
+        echo "Data Gagal Disimpan";
+    }
